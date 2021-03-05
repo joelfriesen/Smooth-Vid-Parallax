054d73413f5dbef65fad983ccace3366c59489c2
Joel <joel.friesen@hakaimagazine.com>
2021-03-04 22:40:02 -0800

    New files. works

--- src/script.js
+++ src/script.js
@@ -4,7 +4,6 @@ $(window).on('scroll resize load', parallax);
 // Clickable artifacts --------------------------------------------------
 //-----------------------------------------------------------------------
 
-
 $(".artifact").click(function () {
   if ($(this).hasClass("hovered")) {
     $(this).removeClass("hovered");
@@ -20,7 +19,6 @@ $(".artifactlink").click(function (event) {
   $("label[for='" + event.target.id + "']").addClass("hovered")
 });
 
-
 //-----------------------------------------------------------------------
 // Clickable Links ------------------------------------------------------
 //-----------------------------------------------------------------------
@@ -33,11 +31,12 @@ function scrollToAnchor(aid) {
   var aTag = $("a[name='" + aid + "']");
   $('html,body').animate({ scrollTop: aTag.offset().top }, 'slow');
 }
-
-
-var filmstop = 30; // The precentage you want the film to finish in
-var filmlength = 300; //Slice up the film and put into the directory using a slicing tool like
-var transition = 10; //Time between video and content
+  
+var imagesize = 3690; // This is needed for the hole calculations to figure out when the bottom is at the bottom of the screen
+var filmstop = 30; // The percent you want the transition to end
+var filmlength = 330; //Slice up the film and put into the directory using a slicing tool
+var transition =10; //percent for legnth of transition
+var footer =90; //percent for starting the footer
 
 for (i = 1; i < filmlength + 1; i++) {
   //Make the frames, file starts at #001
@@ -51,13 +50,15 @@ for (i = 1; i < filmlength + 1; i++) {
 function parallax() {
   //
   //the heart beat
-  var top = $(window).scrollTop();
-  var screen = $(window).height();
-  var webpage = $(document).height();
-  var percentage = top / (webpage - screen) * 100; 
+  var legnthadjustment = 110;  // This is the calculation in percentage adjustment - you might need to tweak something at the end
+  var top = $(window).scrollTop();  // in px, how far is the top of the document, from the top of the screen
+  var screen = $(window).height(); // in px, how tall your screen is
+  var webpage = $(document).height(); // in px, how tall the total document is. We set this in HV in the CSS, on the body tag.
+  var percentage = top / (webpage - screen) * legnthadjustment; // what percent of the document is showing in the screen
 
-  console.clear();
-  console.log("top: " + top + " / (  webpage: " + webpage + " - screen: " + screen + ") = percentage:" + percentage)
+
+  // console.clear();
+  // console.log("top: " + top + " / (  webpage: " + webpage + " - screen: " + screen + ") = percentage:" + percentage)
 
   // Run the video
   if (percentage < filmstop) {
@@ -73,32 +74,42 @@ function parallax() {
     }
     //$(".intro").css("padding-top", percentage*10 + "vh");
   }
+  if (percentage > filmstop) {
+    for (i = 1; i < filmlength + 1; i++) {
+      $('.frame-' + i).removeClass("invis");
+    }
+    //$(".intro").css("padding-top", percentage*10 + "vh");
+  }
+
+  // before transition
   if (percentage < filmstop - transition) {
-    $(".parallax-bg1,.parallax-bg2a,.parallax-bg2b,.parallax-bg3a,.parallax-bg3b,.artifacts,.timeline,.articles").css("top", "100vh");
-    $(".parallax-bg1,.parallax-bg2a,.parallax-bg3a,.parallax-bg2b,.parallax-bg3b").css("background-position", "0 0")
-    console.log("before transition")
+    $(".parallax-bg1,.parallax-bg2a,.parallax-bg2b,.parallax-bg3a,.parallax-bg3b,.artifacts,.timeline").css("top", "100vh");
+    $(".parallax-bg1,.parallax-bg2a,.parallax-bg2b,.parallax-bg3a,.parallax-bg3b,.artifacts,.timeline").css("bottom", "unset")
+  
+  // transition
   } else if ((percentage > filmstop - transition) && (percentage < (filmstop))) {
-    $(".parallax-bg1,.parallax-bg2a,.parallax-bg2b,.parallax-bg3a,.parallax-bg3b,.artifacts,.timeline,.articles").css("top", (-(percentage - filmstop) * transition) + "vh")
-    $(".parallax-bg2a,.parallax-bg3a,.parallax-bg2b,.parallax-bg3b").css("background-position", "0 0")
+    $(".parallax-bg1,.parallax-bg2a,.parallax-bg2b,.parallax-bg3a,.parallax-bg3b,.artifacts,.timeline").css("top", (-(percentage - filmstop) * transition) + "vh")
+    $(".parallax-bg1,.parallax-bg2a,.parallax-bg2b,.parallax-bg3a,.parallax-bg3b,.artifacts,.timeline").css("bottom", "unset")
     $(".parallax-bg1").css("background-position", "50% 0")
-    console.log(" transition")
-  } else if (percentage > filmstop) {
-    var speedofrock = (percentage-filmstop)*(100/(100-filmstop))*36.9; //This is 0-100 from the point where the transition stops
 
-    $(".parallax-bg1,.parallax-bg2a,.parallax-bg2b,.parallax-bg3a,.parallax-bg3b,.articles").css("top", "0vh")
-    $(".parallax-bg1").css("background-position", "50% " + -speedofrock + "px"); //The image is 3690px tall, and I want to scroll it up to the point where it's finished at 100%
-
-    $(".parallax-bg2a").css("background-position", "100% " + -speedofrock * 2 + "px");    //B R
-    $(".parallax-bg2b").css("background-position", "0 " + -speedofrock  * 2+ "px");       //B L
-    $(".parallax-bg3a").css("background-position", "100% " + -speedofrock  * 3+ "px");    //F L
-    $(".parallax-bg3b").css("background-position", "0 " + -speedofrock  * 3+ "px");       //F R
+  // after transition
+  } else if ((percentage > filmstop)&& (percentage < footer)) {
+    $(".parallax-bg1,.parallax-bg2a,.parallax-bg2b,.parallax-bg3a,.parallax-bg3b,.artifacts,.timeline").css("top", "0")
+    $(".parallax-bg1").css("background-position", "50% " + -((percentage-transition-filmstop/percentage)*((imagesize - screen)/100)) + "px");
+    $(".artifacts").css("top", -((percentage-transition-filmstop/percentage)*((imagesize - screen)/100))  + "px");
+    $(".timeline").css("top", -((percentage-transition-filmstop/percentage)*((imagesize - screen)/100))   + "px");
+    $(".parallax-bg2a, .parallax-bg2b").css("background-position", "0 " + -((percentage-transition-filmstop/percentage)*((imagesize - screen)/100)) * 2 + "px");       //B L
+    $(".parallax-bg3a, .parallax-bg3b").css("background-position", "0 " + -((percentage-transition-filmstop/percentage)*((imagesize - screen)/100)) * 3 + "px");       //F R
     
-    $(".artifacts").css("top", -speedofrock + "px");
-    $(".timeline").css("top", -speedofrock + "px");
-    $(".articles").css("top", -speedofrock + "px");
-
-    
+  } else if (percentage > footer) {
+    $(".parallax-bg1,.parallax-bg2a,.parallax-bg2b,.parallax-bg3a,.parallax-bg3b,.artifacts,.timeline").css("top", "0")
     $('.frame-' + filmlength).removeClass("invis");
-    console.log("after transition")
+    $(".parallax-bg1").css("background-position", "50% " + -((percentage-transition-filmstop/percentage)*((imagesize - screen)/100)) + "px");
+    $(".artifacts").css("top", -((percentage-transition-filmstop/percentage)*((imagesize - screen)/100))  + "px");
+    $(".timeline").css("top", -((percentage-transition-filmstop/percentage)*((imagesize - screen)/100))   + "px");
+    $(".parallax-bg2a, .parallax-bg2b").css("background-position", "0 " + -((percentage-transition-filmstop/percentage)*((imagesize - screen)/100)) * 2 + "px");       //B L
+    $(".parallax-bg3a, .parallax-bg3b").css("background-position", "0 " + -((percentage-transition-filmstop/percentage)*((imagesize - screen)/100)) * 3 + "px");       //F R
+    
+    $(".footer").css("bottom",  0 + "vh");       
   }
 }
